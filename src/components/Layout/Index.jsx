3f6822d197523f1d0c97b651e5885a692e4c870d
Mason Yates <Tidalabysstv@gmail.com>
2023-10-27 05:23:56 -0500

    added some styling to the page and started Home

--- src/components/Layout/Index.jsx
+++ src/components/Layout/Index.jsx
@@ -1,10 +1,26 @@
 import './index.scss';
 import Navbar from '../Navbar/Index.jsx';
+import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
   return (
     <>
       <Navbar />
+      <div className='page'>
+        <span className='tags top-tags'>
+          &lt;body&gt;
+          <br />
+          <span className='bottom-tag-html'>&lt;/html&gt;</span>
+          </span>
+
+        <Outlet />
+
+        <span className='tags bottom-tags'>
+          &lt;/link&gt;
+          <br />
+          <span className='script-tag-html'>&lt;/script&gt;</span>
+        </span>
+      </div>
     </>
   )
 }
