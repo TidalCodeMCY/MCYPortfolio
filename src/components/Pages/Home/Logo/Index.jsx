cf2f2d7ab753dca48c85a62cb814f2a405a1ffcc
Mason Yates <Tidalabysstv@gmail.com>
2023-10-28 20:12:03 -0500

    updated some settings on homepage

--- src/components/Pages/Home/Logo/Index.jsx
+++ src/components/Pages/Home/Logo/Index.jsx
@@ -1,12 +1,9 @@
 import gsap from 'gsap'
 import LogoM from '../../../../assets/images/mcy-logo.png'
 import './index.scss'
-import { useEffect } from 'react'
 
 export default function Logo() {
-  const timeline = gsap.timeline({defaults: { duration: 2}})
-    useEffect(()=>{
-      
+  const timeline = gsap.timeline({defaults: { duration: 2}})    
       timeline
       .fromTo('.solid-logo', {duration: 1,rotation: 0,opacity:0,scale: 0 },{rotation: 380,opacity:1,scale: .35})
       .fromTo('.solid-logo', {rotation: 20 },{rotation: -420,scale: .65})
@@ -15,7 +12,6 @@ export default function Logo() {
       .fromTo('.solid-logo', {rotation: -40},{scale: .95,rotation: 40})
       .fromTo('.solid-logo', {rotation: 40},{scale: .85, rotation: -30})
       .fromTo('.solid-logo', {rotation: -30},{scale: .80, rotation: 10})
-    })
     
         return (
     <div className='logo-container'>
