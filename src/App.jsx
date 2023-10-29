5d651b664dc740dc5de91a01ee11db24885e8ce2
Mason Yates <Tidalabysstv@gmail.com>
2023-10-29 06:31:27 -0500

    worked on contact page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout/Index.jsx'
 import Home from './components/Pages/Home/Index.jsx'
 import About from './components/Pages/About/Index.jsx'
+import Contact from './components/Pages/Contact/Index.jsx'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />}></Route>
           <Route path='about' element={<About/>}></Route>
+          <Route path='contact' element={<Contact/>}></Route>
         </Route>
       </Routes>
     </>
--- src/components/Pages/Contact/Index.jsx
+++ src/components/Pages/Contact/Index.jsx
@@ -1,1 +1,65 @@
-import './index.scss'
+import Loader from 'react-loaders';
+import './index.scss';
+import AnimatedLetters from '../../AnimatedLetters/Index.jsx'
+import { useState,useEffect } from 'react';
+
+export default function Contact() {
+  const[letterClass, setLetterClass] = useState('text-animate');
+
+  const contact = ['C','o','n','t','a','c','t','','M','e',];
+  const line1 = ['I','am','','interested','','in','','freelance,','','start-up,','','or','','any','','other','','work','','involving','','web,','','application,']
+  const line2 = ['or','','robotics','development','.','I','would','','love','','to','','be','','part','','of','','some','','larger','','projects','','if','','needed','.']
+  const line3 = ['If','','you','','may','','have','','more','','requests','','or','','questions','','please','','do','','not','','hesitate','','to','','let']
+  const line4 = ['me','','know','','using','','the','','form','','below','.','Have','','a','','wonderful','','day','!']
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover done')
+    },2800)
+  },[])
+
+  return (
+    <>
+      <div className='container contact-page'>
+        <div className='text-zone'>
+          <h1>
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={contact}
+                idx={15}
+              />
+          </h1>
+          <p>
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={line1}
+                idx={40}
+              />
+          </p>
+          <p>
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={line2}
+                idx={80}
+              />
+          </p>
+          <p>
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={line3}
+                idx={120}
+              />
+          </p>
+          <p>
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={line4}
+                idx={160}
+              />
+          </p>
+        </div>
+      </div>
+      <Loader type='pacman' />
+    </>
+  )
+}
