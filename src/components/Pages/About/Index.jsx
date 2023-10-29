990049a109ceaeaaa5b02d8bdeb8fd5c6f5e4eec
Mason Yates <Tidalabysstv@gmail.com>
2023-10-29 04:03:28 -0500

    started work on animated cube

--- src/components/Pages/About/Index.jsx
+++ src/components/Pages/About/Index.jsx
@@ -1,6 +1,8 @@
 import './index.scss'
 import AnimatedLetters from '../../AnimatedLetters/Index.jsx'
 import { useState,useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngular, faCss3, faHtml5, faJsSquare, faNodeJs, faReact } from '@fortawesome/free-brands-svg-icons';
 
 export default function About() {
   const[letterClass, setLetterClass] = useState('text-animate');
@@ -30,7 +32,7 @@ export default function About() {
   useEffect(() => {
     setTimeout(() => {
       setLetterClass('text-animate-hover')
-    },2500)
+    },3000)
   },[])
 
   return (
@@ -100,6 +102,29 @@ export default function About() {
               idx={320}
             />
           </p>
+      </div>
+
+      <div className='stage-cube-cont'>
+        <div className='cubespinner'>
+          <div className='face1'>
+            <FontAwesomeIcon icon={faAngular} color='#DD0031' />
+          </div>
+          <div className='face2'>
+            <FontAwesomeIcon icon={faHtml5} color='#F06529' />
+          </div>
+          <div className='face3'>
+            <FontAwesomeIcon icon={faCss3} color='#28A4D9' />
+          </div>
+          <div className='face4'>
+            <FontAwesomeIcon icon={faReact} color='#5ED4F4' />
+          </div>
+          <div className='face5'>
+            <FontAwesomeIcon icon={faJsSquare} color='#EFD81D' />
+          </div>
+          <div className='face6'>
+            <FontAwesomeIcon icon={faNodeJs} color='#44883e' />
+          </div>
+        </div>
       </div>
     </div>
   )
